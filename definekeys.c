b98d7974463e7170790428dfd7ec7ab27ab6eb10
Reinier Ladan <reinierl@gmail.com>
2021-10-07 13:20:52 +0200

    Keymap agnosticed

---
+++ definekeys.c
@@ -0,0 +1,206 @@
+// # Layer 0
+
+// Top row
+
+#define KR_0_1_1 KC_Q
+#define KR_0_1_2 KC_W
+#define KR_0_1_3 KC_E
+#define KR_0_1_4 KC_R
+#define KR_0_1_5 KC_T
+//
+#define KR_0_1_6 KC_Y
+#define KR_0_1_7 KC_U
+#define KR_0_1_8 KC_I
+#define KR_0_1_9 KC_O
+#define KR_0_1_10 KC_P
+
+// Mid row
+
+#define KR_0_2_1 SFT_T(KC_A)
+#define KR_0_2_2 LCTL_T(KC_S)
+#define KR_0_2_3 LALT_T(KC_D)
+#define KR_0_2_4 LGUI_T(KC_F)
+#define KR_0_2_5 KC_G
+//
+#define KR_0_2_6 KC_H
+#define KR_0_2_7 RGUI_T(KC_J)
+#define KR_0_2_8 RALT_T(KC_K)
+#define KR_0_2_9 RCTL_T(KC_L)
+#define KR_0_2_10 RSFT_T(KC_BSPC)
+
+// Bottom row
+
+#define KR_0_3_1 KC_Z
+#define KR_0_3_2 KC_X
+#define KR_0_3_3 KC_C
+#define KR_0_3_4 KC_V
+#define KR_0_3_5 KC_B
+//
+#define KR_0_3_6 KC_N
+#define KR_0_3_7 KC_M
+#define KR_0_3_8 KC_COMM
+#define KR_0_3_9 TD(DOT_EL)
+#define KR_0_3_10 KC_SLSH
+
+// Thumb cluster
+
+#define KR_0_4_1 KC_MEH
+#define KR_0_4_2 KC_SPC
+#define KR_0_4_3 KC_HYPR
+//
+#define KR_0_4_4 TT(2)
+#define KR_0_4_5 OSL(1)
+#define KR_0_4_6 TT(3)
+
+
+
+// ###### Layer 1
+
+#define KR_1_1_1 KC_ESC
+#define KR_1_1_2 KC_DEL
+#define KR_1_1_3 TD(EUR_DOL)
+#define KR_1_1_4 KC_HASH
+#define KR_1_1_5 KC_UNDS
+//
+#define KR_1_1_6 KC_GRV
+#define KR_1_1_7 KC_QUOT
+#define KR_1_1_8 KC_DQUO
+#define KR_1_1_9 KC_TILD
+#define KR_1_1_10 KC_NO
+
+
+#define KR_1_2_1 SFT_T(KC_A)
+#define KR_1_2_2 LCTL_T(KC_AMPR)
+#define KR_1_2_3 LALT_T(KC_SCLN)
+#define KR_1_2_4 TD(CMD_EXL)
+#define KR_1_2_5 KC_COLN
+//
+#define KR_1_2_6 KC_NO
+#define KR_1_2_7 RGUI_T(KC_LBRC)
+#define KR_1_2_8 RALT_T(KC_RBRC)
+#define KR_1_2_9 RCTL_T(KC_MINUS)
+#define KR_1_2_10 RSFT_T(KC_BSPC)
+
+
+#define KR_1_3_1 LALT(KC_BSLS)
+#define KR_1_3_2 LSA(KC_BSLS)
+#define KR_1_3_3 LALT(KC_8)
+#define KR_1_3_4 TD(PI_PASTE)
+#define KR_1_3_5 LSFT(KC_2)
+//
+#define KR_1_3_6 KC_NO
+#define KR_1_3_7 LSFT(KC_LBRC)
+#define KR_1_3_8 LSFT(KC_RBRC)
+#define KR_1_3_9 KC_CIRC
+#define KR_1_3_10 KC_BSLASH
+
+
+#define KR_1_4_1 KC_MEH
+#define KR_1_4_2 KC_SPC
+#define KR_1_4_3 KC_HYPR
+//
+#define KR_1_4_4 TT(2)
+#define KR_1_4_5 OSL(1)
+#define KR_1_4_6 TT(3)
+
+
+
+// ##### Layer 2
+
+#define KR_2_1_1 KC_ESC
+#define KR_2_1_2 KC_DEL
+#define KR_2_1_3 KC_NO
+#define KR_2_1_4 LGUI(KC_GRV)
+#define KR_2_1_5 LCTL(KC_TAB)
+//
+#define KR_2_1_6 KC_PGUP
+#define KR_2_1_7 LGUI(KC_LBRC)
+#define KR_2_1_8 KC_UP
+#define KR_2_1_9 LGUI(KC_RBRC)
+#define KR_2_1_10 KC_NO
+
+
+#define KR_2_2_1 KC_LSFT
+#define KR_2_2_2 KC_LCTL
+#define KR_2_2_3 KC_LOPT
+#define KR_2_2_4 KC_LCMD
+#define KR_2_2_5 KC_NO
+//
+#define KR_2_2_6 KC_PGDN
+#define KR_2_2_7 KC_LEFT
+#define KR_2_2_8 KC_DOWN
+#define KR_2_2_9 KC_RGHT
+#define KR_2_2_10 RSFT_T(KC_BSPC)
+
+
+#define KR_2_3_1 KC_NO
+#define KR_2_3_2 LGUI(KC_X)
+#define KR_2_3_3 LGUI(KC_C)
+#define KR_2_3_4 LGUI(KC_V)
+#define KR_2_3_5 KC_NO
+//
+#define KR_2_3_6 KC_NO
+#define KR_2_3_7 LGUI(KC_MINUS)
+#define KR_2_3_8 LGUI(KC_EQL)
+#define KR_2_3_9 KC_NO
+#define KR_2_3_10 KC_NO
+
+
+#define KR_2_4_1 TO(0)
+#define KR_2_4_2 KC_SPC
+#define KR_2_4_3 KC_HYPR
+//
+#define KR_2_4_4 TT(2)
+#define KR_2_4_5 OSL(1)
+#define KR_2_4_6 TT(3)
+
+
+
+// ##### Layer 3
+
+#define KR_3_1_1 TD(ESC_FRC)
+#define KR_3_1_2 KC_DEL
+#define KR_3_1_3 KC_NO
+#define KR_3_1_4 KC_PERC
+#define KR_3_1_5 KC_TAB
+//
+#define KR_3_1_6 KC_DOT
+#define KR_3_1_7 KC_7
+#define KR_3_1_8 KC_8
+#define KR_3_1_9 KC_9
+#define KR_3_1_10 KC_NO
+
+
+#define KR_3_2_1 KC_LSFT
+#define KR_3_2_2 KC_LCTL
+#define KR_3_2_3 LALT_T(KC_PMNS)
+#define KR_3_2_4 LGUI_T(KC_PPLS)
+#define KR_3_2_5 KC_COLN
+//
+#define KR_3_2_6 KC_COMM
+#define KR_3_2_7 KC_4
+#define KR_3_2_8 KC_5
+#define KR_3_2_9 KC_6
+#define KR_3_2_10 KC_BSPC
+
+
+#define KR_3_3_1 KC_NO
+#define KR_3_3_2 KC_NO
+#define KR_3_3_3 KC_NO
+#define KR_3_3_4 KC_ASTR
+#define KR_3_3_5 KC_PEQL
+//
+#define KR_3_3_6 KC_0
+#define KR_3_3_7 KC_1
+#define KR_3_3_8 KC_2
+#define KR_3_3_9 KC_3
+#define KR_3_3_10 KC_SLSH
+
+
+#define KR_3_4_1 TO(0)
+#define KR_3_4_2 KC_SPC
+#define KR_3_4_3 KC_HYPR
+//
+#define KR_3_4_4 TT(2)
+#define KR_3_4_5 OSL(1)
+#define KR_3_4_6 TT(3)
